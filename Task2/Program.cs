5dd1ea8608239748849f38c2f0322d8c61fa239f
Dmitriy Baranov <bdvbuild@gmail.com>
2022-11-07 13:35:00 +0400

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+             * Знаки препинания не используются. Составить программу, определяющую является ли строка палиндромом 
+             * без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).
+             */
+
+            string str1 = Console.ReadLine();
+            string strTemp = str1.ToLower().Replace(" ", "");
+            string str2 = "";
+            foreach (char c in strTemp)
+            {
+                str2 = c + str2;
+            }
+            if (str2 == strTemp)
+            {
+                Console.WriteLine("Предложение явяется палиндромом");
+            } else
+                Console.WriteLine("Предложение не является палиндромом");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Дана строка S. Из строки требуется удалить текст, заключенный в фигурные скобки. 
+             * В строке может быть несколько фрагментов, заключённых в фигурные скобки. 
+             * Возможно использование вложенных фигурных скобок, необходимо, чтобы программа это учитывала.
+            */
+
+            string str = Console.ReadLine();
+            bool par = str.Contains("{") && str.Contains("}");
+            int n = 0;
+            int k = n+1;
+
+            while (par)
+            {
+                int found1 = str.IndexOf("{", n);
+                int found3 = str.IndexOf("{", found1 + 1);
+                int found2 = str.IndexOf("}", k);
+                if ((found3 > found2) || (found3 == -1))
+                {
+                    str = str.Remove(found1, found2 - found1 + 1);
+                    n = 0;
+                } else n++;
+                par = str.Contains("{") && str.Contains("}");
+            }
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
